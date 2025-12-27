0aa18148234fd2596259f10075922f149d349692
Slava Razum <razum@qruto.to>
2025-12-27 15:07:05 +0100

    [Craft] feat(task-list): enhance deadline display with flag icon and formatted date

--- extensions/craft/src/components/CreateTaskForm.tsx
+++ extensions/craft/src/components/CreateTaskForm.tsx
@@ -69,8 +69,13 @@ export function CreateTaskForm({ onTaskCreated }: CreateTaskFormProps) {
         <Form.Dropdown.Item value="inbox" title="Inbox" icon={Icon.Tray} />
         <Form.Dropdown.Item value="dailyNote" title="Daily Note" icon={Icon.Calendar} />
       </Form.Dropdown>
-      <Form.DatePicker id="scheduleDate" title="Schedule Date" value={scheduleDate} onChange={setScheduleDate} />
-      <Form.DatePicker id="deadlineDate" title="Deadline" value={deadlineDate} onChange={setDeadlineDate} />
+      <Form.DatePicker id="scheduleDate" title="Schedule" value={scheduleDate} onChange={setScheduleDate} />
+      <Form.DatePicker
+        id="deadlineDate"
+        title="Deadline"
+        value={deadlineDate}
+        onChange={setDeadlineDate}
+      />
     </Form>
   );
 }
